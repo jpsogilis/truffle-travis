b3274cb8c6160cb9551be462ea8146a067cb946e
Jp Geslin <jean-pierre@sogilis.com>
2018-03-28 02:41:35 +0200

    Refactor !!!

--- clive/truffle-jenkins.js
+++ clive/truffle-jenkins.js
@@ -3,10 +3,10 @@ require('babel-register')
 module.exports = {
   networks: {
     development: {
-      },
       host: 'ganache',
       port: 8545,
       network_id: '*' // Match any network id
+    }
   },
   mocha: {
     reporter: "mocha-junit-reporter"
